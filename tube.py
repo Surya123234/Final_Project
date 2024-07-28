4242f05bd4155cdfffe021517f626ac2e5ecd737
Surya Sendhilraj <surya.sendhiraj@gmail.com>
2024-07-28 13:41:00 -0400

    print times for all heights and remove weird height calc

--- tube.py
+++ tube.py
@@ -9,8 +9,9 @@ D = 0.00794
 A_TUBE = pi * D**2 / 4
 EPSILON = 0.0000015
 G = 9.81
-L = (0.2, 0.6)
+L = (0.2, 0.3, 0.4, 0.6)
 TOLERANCE = 0.00001
+
 
 def reynolds(v):
     return DENS * v * D / MU
@@ -41,36 +42,38 @@ def alpha(r):
 def get_V2(z1, a, f):
     denom_3 = a * A_TUBE**2 / A_BOX**2
     denom_2 = f * L[0] / D
-    return (2 * G * z1 / (a + denom_2 - denom_3 + 0.5)) ** 0.5
+    return (2 * G * z1 / (a + denom_2 - denom_3)) ** 0.5
 
 
 def get_V1(v2):
     return v2 * A_TUBE / A_BOX
 
+
 def main():
-    h = L[0]
-    h_i = h
-    t = 0
-    # Variables
-    v2 = 0.6755
-    r = reynolds(v2)
-    new_v2 = 0
+    for l in L:
+        h = l
+        h_i = h
+        t = 0
+        # Variables
+        v2 = 0.6755
+        r = reynolds(v2)
+        new_v2 = 0
 
-    while h > 0:
-        i = 0
-        while i < 1000:
-            r = reynolds(v2)
-            new_v2 = get_V2(h+ 0.02 + (L[0] / 150), alpha(r), f(r))
-            if abs(new_v2 - v2) <= TOLERANCE: break 
-            v2 = new_v2
-            i += 1
+        while h > 0:
+            i = 0
+            while i < 1000:
+                r = reynolds(v2)
+                new_v2 = get_V2(h, alpha(r), f(r))
+                if abs(new_v2 - v2) <= TOLERANCE:
+                    break
+                v2 = new_v2
+                i += 1
 
-        v1 = get_V1(v2)
-        t += 0.1
-        h = h_i - t * v1
+            v1 = get_V1(v2)
+            t += 0.1
+            h = h_i - t * v1
 
-    print("Final time", t)
-
+        print(f"Final time for length {l} metres: {int(t)} seconds")
 
 if __name__ == "__main__":
     main()
